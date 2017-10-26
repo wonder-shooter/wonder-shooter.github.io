414b3b0dea9ca3e94ba4c6a8b0174381a2b20305
kamiyam <labs.k5m@gmail.com>
2017-10-27 08:48:44 +0900

    [issues/1] JSON追加処理実装

--- server/index.js
+++ server/index.js
@@ -1,18 +1,45 @@
 const fastify = require('fastify')();
 const path = require('path');
+const fs = require('fs');
+const util = require('util');
+
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
 
 const rootPath = path.join(__dirname, '../');
-const htoocsPath = 'htdocs';
-const scoreDataPath = 'assets/js/score.json';
+const htoocsPath = path.join(rootPath, 'htdocs');
+const scoreDataPath = path.join(htoocsPath, 'assets/js/score.json');
 
 // Routing
 fastify.register(require('fastify-static'), {
-  root: path.join(rootPath, htoocsPath),
+  root: htoocsPath,
 });
 //
 fastify.get('/score', async (request, reply) => {
+  const json = await readFile(scoreDataPath);
   reply.type('application/json').code(200);
-  return { result: 'ok' }
+  return JSON.parse(json);
+});
+//
+fastify.post('/score', async (request, reply) => {
+  try {
+    const data = await readFile(scoreDataPath);
+    const json = JSON.parse(data);
+    const postData = request.body;
+
+    const scores = postData.filter((score) => {
+      return (score.name != null && score.score != null);
+    });
+    Array.prototype.push.apply(json, scores);
+    await writeFile(scoreDataPath, JSON.stringify(json));
+
+    reply.type('application/json').code(200);
+    return { result: 'ok' }
+
+  } catch(e) {
+    console.error(e);
+    return { result: 'form_data invalid'};
+  }
 });
 // Server Listen
 fastify.listen(process.env.NODE_PORT || 3000, err => {
